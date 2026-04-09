367d6325e0148696c8572da625a9a156609d98cc
acquah-forever <kojoaddo598@gmail.com>
2026-04-09 22:38:09 +0000

    edited NavBar.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,8 @@ import Profile from '../assets/bus.jpeg'
 
 const Hero = () => {
     return (
-        <motion.div initial={{ opacity: 0, y: -60 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.4, ease: 'easeInOut' }} className='relative md:min-h-screen bg-[url("https://cdn.pixabay.com/photo/2019/10/03/12/12/javascript-4523100_1280.jpg")] bg-cover bg-center bg-fixed bg-no-repeat'>
+        <motion.div initial={{ opacity: 0, y: -60 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.4, ease: 'easeInOut' }} 
+        className='relative min-h-screen bg-[url("https://cdn.pixabay.com/photo/2016/06/22/11/10/box-1472804_1280.png")] bg-cover bg-center bg-fixed bg-no-repeat'>
             <div className='absolute inset-0 bg-black/30'></div>
             <div className='relative z-50'>
                
