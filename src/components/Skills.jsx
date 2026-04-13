2bfa4485473540931a1a3c0d0f79ec3964a9b53f
acquah-forever <kojoaddo598@gmail.com>
2026-04-13 14:48:15 +0000

    added map functionality for various tech stack

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 
 const Skills = ({ front }) => {
+    const LucideComponent = front.svg
 
     return (
-        <div className='flex'>
-            <div>
-                <div>{front.svg}</div>
-                <div>{front.name}</div>
+        <div className='flex items-center gap-1'>
+            <div className=' text-sky-500'>
+                <LucideComponent size={25} />
             </div>
+            <div className='text-center font-semibold text-sm'>{front.name}</div>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Skills from '../components/Skills'
 import Form from '../components/Form'
 import Footer from '../components/Footer'
 import { getFrontend } from '../data/frontend'
-import{getLearning} from '../data/learning'
+
 
 
 const Home = () => {
@@ -18,11 +18,12 @@ const Home = () => {
       <div className='mx-20'>
         <About />
       </div>
-      <div>
+      <h1 className='text-2xl sm:text-3xl md:text-4xl font-bold  mb-5 sm:mb-7 text-center'>Tech <span className='bg-linear-to-bl from-sky-500 via-violet-300 to-indigo-400 bg-clip-text text-transparent'>Stack</span></h1>
+      <h1 className='font-mono text-2xl font-semibold'>//FrontEnd</h1>
+      <div className='max-w-5xl w-full grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 mt-5'>
         {frontend.map((front) => (<Skills
-        front ={front} key ={front.id}
+          front={front} key={front.id}
         />))}
-        
       </div>
       <Form />
       <Footer />
