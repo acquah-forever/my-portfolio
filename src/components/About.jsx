e68e296f6f17d4a337b762396afa08d334c18f0a
acquah-forever <kojoaddo598@gmail.com>
2026-04-17 12:44:05 +0000

    redefined motion in Hero.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
 
     return (
         <div id='about' className='bg-slate-400/40 mt-20 min-h-screen w-full grid grid-cols-1 space-y-10 justify-items-center items-center lg:grid-cols-2 lg:justify-items-center md:gap-10'>
-            <motion.div variants={parent} initial="hidden" animate="visible" className='md:max-w-xl w-full flex flex-col justify-center px-10 items-center'>
+            <motion.div variants={parent} initial="hidden" whileInView="visible" viewport={{once:false, margin:"-100px"}} className='md:max-w-xl w-full flex flex-col justify-center px-10 items-center'>
 
                 <motion.h1 variants={children} className='text-2xl sm:text-3xl md:text-4xl font-bold text-left mb-5 sm:mb-7'>About <span className='bg-linear-to-bl from-sky-500 via-violet-300 to-indigo-400 bg-clip-text text-transparent'>Me</span></motion.h1>
                 <motion.div variants={children} className='w-full max-w-md'>
@@ -53,7 +53,7 @@ const About = () => {
 
             </motion.div>
 
-            <div className="w-full max-w-lg aspect-4/3  border bg-slate-300/0 md:mx-0">
+            <motion.div variants={parent} initial="hidden" whileInView="visible" viewport={{once:false, margin:"-100px"}} className="w-full max-w-lg aspect-4/3  border bg-slate-300/0 md:mx-0">
                 <div className='flex items-center mt-3'>
                     <div className='px-3 flex space-x-1'>
                         <div className='w-3 h-3 bg-red-400 rounded-full'></div>
@@ -63,16 +63,16 @@ const About = () => {
                 </div>
                 <div className='border-t mt-2 border-slate-400'></div>
                 <div className='mx-1 mt-2 flex flex-wrap gap-1'>
-                    <button className={`px-4 sm:px-5 py-2 sm:py-3 cursor-pointer text-white text-sm sm:text-base rounded-t-xl transition-colors ${activeTab === 'About' ? "bg-sky-500" : "bg-sky-800 hover:bg-sky-400"} transition-all duration-200`} onClick={() => setActiveTab('About')}>About</button>
-                    <button className={`px-4 sm:px-5 py-2 sm:py-3 cursor-pointer text-white text-sm sm:text-base rounded-t-xl transition-colors ${activeTab === 'Comment' ? "bg-sky-500" : "bg-sky-800 hover:bg-sky-400"} transition-all duration-500`} onClick={() => setActiveTab('Comment')}>Comment</button>
+                    <motion.button variants={children} className={`px-4 sm:px-5 py-2 sm:py-3 cursor-pointer text-white text-sm sm:text-base rounded-t-xl transition-colors ${activeTab === 'About' ? "bg-sky-500" : "bg-sky-800 hover:bg-sky-400"} transition-all duration-200`} onClick={() => setActiveTab('About')}>About</motion.button>
+                    <motion.button variants={children} className={`px-4 sm:px-5 py-2 sm:py-3 cursor-pointer text-white text-sm sm:text-base rounded-t-xl transition-colors ${activeTab === 'Comment' ? "bg-sky-500" : "bg-sky-800 hover:bg-sky-400"} transition-all duration-500`} onClick={() => setActiveTab('Comment')}>Comment</motion.button>
                 </div>
-                <div className='mt-1 h-full text-sm sm:text-base'>
+                <motion.div variants={children} className='mt-1 h-full text-sm sm:text-base'>
                     <SyntaxHighlighter language='javascript' style={nightOwl}
                         customStyle={{ margin: 0, width: "100%", height: "100%" }}
                     >{data2[activeTab]}</SyntaxHighlighter>
-                </div>
+                </motion.div>
 
-            </div>
+            </motion.div>
 
         </div>
     )
