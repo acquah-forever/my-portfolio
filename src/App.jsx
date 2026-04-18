060211db8a1c9fefe7c80ada45f12bc6206e90da
acquah-forever <kojoaddo598@gmail.com>
2026-04-18 23:11:43 +0000

    finalised progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import Home from './pages/Home'
 import Footer from './components/Footer'
+import ProgressBar from './components/ProgressBar'
 
 import { Routes, Route } from 'react-router-dom'
 
@@ -11,6 +12,7 @@ const App = () => {
   return (
     <div className='min-h-screen mx-auto bg-slate-200'>
       <NavBar />
+      <ProgressBar />
       <Routes>
         <Route path='/' element={<Home />}/>
       </Routes>
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import useScrollProgress  from '../hooks/useScrollProgress'
 
 const ProgressBar = () => {
+    const[scrollProgress] = useScrollProgress()
   return (
-    <div>
-      
+    <div className='fixed top-0 left-0 w-full h-1 bg-indigo-400 z-50'>
+        <div className='h-full' style={{width : `${scrollProgress}`}}>
+        </div>
     </div>
   )
 }
